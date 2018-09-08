cdab81726ed2e5a41a55de69916dfdad3caad905
Christopher Vensand <chrisvensand@gmail.com>
2018-09-08 18:50:08 -0400

    Added particle application

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
 app.use(express.static(__dirname))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
