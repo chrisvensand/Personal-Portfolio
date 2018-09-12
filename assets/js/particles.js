66a04b673aae58c19f47fd456a596fd2f41b230e
Christopher Vensand <chrisvensand@gmail.com>
2018-09-12 19:46:22 -0400

    Initial commitE

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     particles: {
         number: {
-        value: 175,
+        value: 160,
         density: { enable: true, value_area: 946.9921162906311 }
         },
         color: { value: "#004aff" },
